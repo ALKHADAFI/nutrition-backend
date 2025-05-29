d140709ad13efe822f07e9f13b89fb029cf9249d
Mamak Martani Putro <133627700+ALKHADAFI@users.noreply.github.com>
2025-05-29 12:30:10 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,23 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.mongodb+srv://alkhadafi:bS1hYBr9Z1ps4Pgm@cluster0.n61pkfd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0);
-    console.log('MongoDB Connected');
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
+const uri = process.env.MONGO_URI;
+
+if (!uri) {
+console.error('❌ Environment variable MONGO_URI tidak ditemukan.');
+process.exit(1);
+}
+
+try {
+await mongoose.connect(uri, {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+});
+console.log('✅ MongoDB berhasil terkoneksi.');
+} catch (err) {
+console.error('❌ Gagal terkoneksi ke MongoDB:', err.message);
+process.exit(1);
+}
 };
 
 module.exports = connectDB;
