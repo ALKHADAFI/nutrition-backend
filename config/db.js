fda9a55fc0ff6d1611c8431dc3291a2b70b40918
Mamak Martani Putro <133627700+ALKHADAFI@users.noreply.github.com>
2025-05-29 12:16:08 +0700

    Update db.js
    penambahan URImongoDB

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.mongodb+srv://alkhadafi:bS1hYBr9Z1ps4Pgm@cluster0.n61pkfd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0);
     console.log('MongoDB Connected');
   } catch (err) {
     console.error(err);
