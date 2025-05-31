2871d1d4588db2b598ecba8a6d3e1392e7aa63c4
Mamak Martani Putro <133627700+ALKHADAFI@users.noreply.github.com>
2025-05-31 11:36:44 +0700

    Update fetchFromOpenFoodFacts.js

--- helpers/fetchFromOpenFoodFacts.js
+++ helpers/fetchFromOpenFoodFacts.js
@@ -3,27 +3,31 @@ const evaluateHealth = require('./evaluateHealth');
 
 async function fetchFromOpenFoodFacts(barcode) {
 try {
-const url = `https://world.openfoodfacts.org/api/v0/product/${barcode}.json`;
+const url = https:`//world.openfoodfacts.org/api/v0/product/${barcode}.json`;
 const response = await axios.get(url);
-if (response.data.status === 1) {
+  if (response.data.status === 1) {
   const product = response.data.product;
+  const nutriments = product.nutriments || {};
 
-  // Konversi ke format skema MongoDB Anda
+  const sugar = nutriments.sugars_100g || 0;
+  const calories = nutriments.energy_kcal_100g || 0;
+  const saturatedFat = nutriments["saturated-fat_100g"] || 0;
+  const sodiumGrams = nutriments.sodium_100g || 0;
+  const sodium = sodiumGrams * 1000; // convert gram to mg
+
   return {
     barcode: product.code,
     name: product.product_name || 'Tidak diketahui',
     brand: product.brands || '',
-    sugar: product.nutriments?.sugars_100g || 0,
-    calories: product.nutriments?.energy_kcal_100g || 0,
-    healthyFor: evaluateHealth({
-      sugar: product.nutriments?.sugars_100g || 0,
-      calories: product.nutriments?.energy_kcal_100g || 0
-    })
-};
+    sugar,
+    calories,
+    saturatedFat,
+    sodium,
+    healthyFor: evaluateHealth({ sugar, calories, saturatedFat, sodium })
+  };
 } else {
   return null;
 }
-
 } catch (err) {
 console.error('❌ Error saat fetch dari Open Food Facts:', err.message);
 return null;
