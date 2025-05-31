1e2f4d99d516989640616a2b44d624878a55af61
Mamak Martani Putro <133627700+ALKHADAFI@users.noreply.github.com>
2025-05-31 11:32:54 +0700

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,35 +1,42 @@
-const express = require('express');
-const router = express.Router();
-const Product = require('../models/Product');
-const fetchFromOpenFoodFacts = require('../helpers/fetchFromOpenFoodFacts');
-const evaluateHealth = require('../helpers/evaluateHealth');
+const mongoose = require('mongoose');
 
-router.get('/:barcode', async (req, res) => {
-  const barcode = req.params.barcode;
-
-  try {
-    let product = await Product.findOne({ barcode });
-
-    if (!product) {
-      const offData = await fetchFromOpenFoodFacts(barcode);
-
-      if (offData) {
-        offData.healthyFor = evaluateHealth({
-          sugar: offData.sugar,
-          calories: offData.calories,
-        });
-
-        product = new Product(offData);
-        await product.save();
-      } else {
-        return res.status(404).json({ message: 'Produk tidak ditemukan.' });
-      }
-    }
-
-    res.json(product);
-  } catch (err) {
-    res.status(500).json({ message: 'Terjadi kesalahan server' });
+const ProductSchema = new mongoose.Schema({
+  barcode: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  brand: {
+    type: String,
+    default: ''
+  },
+  sugar: {
+    type: Number,
+    default: 0
+  },
+  calories: {
+    type: Number,
+    default: 0
+  },
+saturatedFat: {
+  type: Number,
+  default: 0 // dalam gram
+    },
+  sodium: {
+    type: Number,
+    default: 0 // dalam miligram (mg)
+      },
+  healthyFor: {
+    type: [String],
+    default: []
   }
+}, {
+  timestamps: true
 });
 
-module.exports = router;
+module.exports = mongoose.model('Product', ProductSchema);
