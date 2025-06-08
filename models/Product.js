f7afa9bebef98d864d33455c1e956a5a1d28fe34
Mamak Martani Putro <133627700+ALKHADAFI@users.noreply.github.com>
2025-06-08 10:45:37 +0700

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,12 +1,10 @@
-const mongoose = require('mongoose');
-
 const ProductSchema = new mongoose.Schema({
   barcode: { type: String, required: true, unique: true },
   name: String,
   brand: String,
   sugar: Number,
   calories: Number,
+  saturatedFat: Number,
+  sodium: Number,
   healthyFor: [String]
 });
-
-module.exports = mongoose.model('Product', ProductSchema);
